65bf6cd7fc1f18ae7aa0df000412c31de12ec635
lobster <ajjahdb94@gmail.com>
2026-06-20 17:26:25 +0700

    test commit

--- main.lua
+++ main.lua
@@ -2,15 +2,27 @@ local http = game:GetService("HttpService")
 local players = game:GetService("Players")
 local replicated_storage = game:GetService("ReplicatedStorage")
 
+local REPO = "https://raw.githubusercontent.com/LobsterFromBoston/jailbreak-tracker/master/"
+
 local function load(path)
-	return loadstring(readfile(path))()
+	local response = request({
+		Url = REPO .. path,
+		Method = "GET",
+	})
+
+	local fn, err = loadstring(response.Body)
+	if not fn then
+		error("Failed to load " .. path .. ": " .. tostring(err))
+	end
+
+	return fn()
 end
 
-local webhook_map = load("tracker/discord/webhooks.lua")
-local role_map = load("tracker/discord/roles.lua")
-local send_webhook = load("tracker/discord/send.lua")(http)
-local status_map = load("tracker/robbery/status_map.lua")
-local tracker = load("tracker/robbery/tracker.lua")
+local webhook_map = load("discord/webhooks.lua")
+local role_map = load("discord/roles.lua")
+local send_webhook = load("discord/send.lua")(http)
+local status_map = load("robbery/status_map.lua")
+local tracker = load("robbery/tracker.lua")
 
 local job_id = game.JobId
 local player_count = #players:GetPlayers()
---
+++ robbery/status_map.lua
@@ -0,0 +1,12 @@
+return {
+	status = {
+		[1] = "open",
+		[2] = "in robbery",
+		[3] = "closed",
+	},
+	colors = {
+		["open"] = 0x2ecc71,
+		["in robbery"] = 0xe74c3c,
+		["closed"] = 0x95a5a6,
+	},
+}
